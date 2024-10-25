80833ee850a35920f1d60203858437ab070e44b4
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-10-25 17:55:31 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -6,19 +6,19 @@ const TranslationContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color:white;
-  height:100vh ;
+  background-color: white;
+  height: 100vh;
 `;
 
 const CameraPlaceholder = styled.div`
   width: 80%;
-  height: 50vh; 
+  height: 50vh;
   margin-top: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color:black;
-`;  
+  background-color: black;
+`;
 
 const CameraFeed = styled.img`
   max-width: 100%;
@@ -28,7 +28,7 @@ const CameraFeed = styled.img`
 const TranslationText = styled.div`
   margin-top: 2rem;
   font-size: 1.5rem;
-  color:black;
+  color: black;
 
   @media (max-width: 768px) {
     font-size: 1.2rem;
@@ -39,7 +39,7 @@ const Instructions = styled.div`
   margin-top: 2rem;
   font-size: 1.2rem;
   text-align: center;
-  color:black;
+  color: black;
 
   @media (max-width: 768px) {
     font-size: 1rem;
@@ -56,18 +56,16 @@ function ASLTranslationPage() {
   const [cameraImage, setCameraImage] = useState('');
   const [translation, setTranslation] = useState('');
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/translate');
+        const response = await fetch('https://flasky-d9sr.onrender.com/translate');
         if (!response.ok) {
           throw new Error('Failed to fetch');
         }
         const data = await response.json();
         setCameraImage(data.img);
         if (data.translation !== '') {
-          // Append the new translation to the existing one
           setTranslation(prevTranslation => prevTranslation + data.translation);
         }
       } catch (error) {
