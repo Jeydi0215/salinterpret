7f996e1703f1996cb912847b9220f065b553901c
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-07 23:03:04 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect, useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
 import styled from 'styled-components';
 
 const TranslationContainer = styled.div`
