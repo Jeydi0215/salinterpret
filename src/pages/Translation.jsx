190b9975b6a6040bb39896daa52cff22532ab9f5
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-07 13:26:32 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -141,7 +141,7 @@ function ASLTranslationPage() {
   // Fetch translation based on the captured image
   const fetchTranslation = async (image) => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/translate', {
+      const response = await fetch('https://flask-server-sptz.onrender.com/translate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
