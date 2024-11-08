16144073f7a56f1572c2d3e4b5909c60c1dfa539
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-08 17:27:37 +0800

    Update AdminNavbar.jsx

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -10,6 +10,7 @@ export default function Navbar({ isScrolled }) {
   const [isMobile, setIsMobile] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const links = [
+     { name: 'Main', link: '../Main'},
     { name: 'Upload', link: '../upload'},
   ];
 
