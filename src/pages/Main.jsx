b137dec260bb3c0d5d03e47a275a5e3d55af2871
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-08 17:20:48 +0800

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,74 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import UserNavbar from '../components/AdminNavbar';
-import { getQuizAnalytics } from '../utils/Service'; // Import the function
+// // src/pages/AdminPage.js
+// import React from 'react';
+// import styled from 'styled-components';
+// import UserNavbar from '../components/AdminNavbar';
 
-const AdminPage = () => {
-  const [analyticsData, setAnalyticsData] = useState(null);
+// const AdminPage = () => {
+//   return (
+//     <Container>
+//       <UserNavbar isScrolled={false} /> 
+//       <Content>
+//         <Header>
+//         </Header>
+//         <MainContent>
+//           <ContentArea>
+//             <h2>Data Overview</h2>
+//             <p>Welcome to the admin dashboard!</p>
+//             {/* Add your admin content here */}
+//           </ContentArea>
+//         </MainContent>
+//       </Content>
+//     </Container>
+//   );
+// };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getQuizAnalytics();
-      setAnalyticsData(data);
-    };
+// const Container = styled.div`
+//   background-color: #1b1212;
+//   height: 100vh;
+// `;
 
-    fetchData();
-  }, []);
+// const Content = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   height: 100%;
+// `;
 
-  return (
-    <Container>
-      <UserNavbar isScrolled={false} />
-      <Content>
-        <Header>
-          <h1>Admin Dashboard</h1>
-        </Header>
-        <MainContent>
-          <ContentArea>
-            <h2>Data Overview</h2>
-            {analyticsData ? (
-              <div>
-                <h3>Quiz Analytics</h3>
-                <pre>{JSON.stringify(analyticsData, null, 2)}</pre> {/* You can format the data to display as per your need */}
-              </div>
-            ) : (
-              <p>Loading analytics data...</p>
-            )}
-          </ContentArea>
-        </MainContent>
-      </Content>
-    </Container>
-  );
-};
+// const Header = styled.header`
+//   background: #141414;
+//   color: white;
+//   padding: 20px;
+//   text-align: center;
+//   font-size: 1.5em;
+//   top:0px;
+// `;
 
-// Styled components
-const Container = styled.div`
-  background-color: #1b1212;
-  height: 100vh;
-`;
+// const MainContent = styled.div`
+//   display: flex;
+//   flex: 1;
+//   margin-top:50px;
+// `;
 
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
 
-const Header = styled.header`
-  background: #141414;
-  color: white;
-  padding: 20px;
-  text-align: center;
-  font-size: 1.5em;
-`;
+// const ContentArea = styled.div`
+//   flex: 1;
+//   padding: 20px;
+// `;
 
-const MainContent = styled.div`
-  display: flex;
-  flex: 1;
-  margin-top: 100px;
-`;
-
-const ContentArea = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-export default AdminPage;
+// export default AdminPage;
