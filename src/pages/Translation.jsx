dbcf9f0bab616eb0426b0600131f6529b3265350
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-08 16:22:05 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -1,143 +1,389 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Navbar from '../components/UserNavbar';
+import { useInView } from 'react-intersection-observer';
+import ASL from '../assets/logo.png';
+import BackgroundImage from '../assets/lopit.png';
+import KaliwaLogo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import Hacker from '../assets/Hacker.png';
+import Hispter from '../assets/Hipster.png';
+import Member from '../assets/Member.png';
+import Mentor from '../assets/Mentor.png';
+import { Analytics } from '@vercel/analytics/react';
 
-const TranslationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-  height: 100vh;
-`;
-
-const CameraPlaceholder = styled.div`
-  width: 80%;
-  height: 50vh;
-  margin-top: 15vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-`;
-
-const CameraFeed = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-`;
-
-const TranslationText = styled.div`
-  margin-top: 2rem;
-  font-size: 1.5rem;
-  color: black;
-
-  @media (max-width: 768px) {
-    font-size: 1.2rem;
+const scrollToSection = (id) => {
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
   }
-`;
+};
 
-const Instructions = styled.div`
-  margin-top: 2rem;
-  font-size: 1.2rem;
-  text-align: center;
-  color: black;
+const Navbar = ({ onSeeMoreClick }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-  @media (max-width: 768px) {
-    font-size: 1rem;
-  }
-`;
-
-const CaptureButton = styled.button`
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-`;
-
-const ClearButton = styled.button`
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-`;
-
-function ASLTranslationPage() {
-  const videoRef = useRef(null);
-  const [translation, setTranslation] = useState('');
-
-  // Function to start the webcam stream
-  useEffect(() => {
-    if (videoRef.current) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-        })
-        .catch((err) => {
-          console.error("Error accessing webcam: ", err);
-        });
-    }
-  }, []);
-
-  // Capture a snapshot when the "Capture" button is clicked
-  const captureSnapshot = () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-
-    // Convert the canvas image to a Blob for uploading as FormData
-    canvas.toBlob((blob) => {
-      fetchTranslation(blob);
-    }, 'image/png');
-  };
-
-  // Fetch translation based on the captured image
-  const fetchTranslation = async (imageBlob) => {
-    try {
-      const formData = new FormData();
-      formData.append('image', imageBlob);
-
-      const response = await fetch('https://flasky-d9sr.onrender.com/translate', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch translation');
-      }
-
-      const data = await response.json();
-      if (data.translation) {
-        setTranslation((prevTranslation) => prevTranslation + data.translation);
-      }
-    } catch (error) {
-      console.error('Error fetching translation:', error.message);
-    }
-  };
-
-  const handleClearTranslation = () => {
-    setTranslation('');
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <TranslationContainer>
-      <Navbar />
-      <CameraPlaceholder>
-        <video ref={videoRef} autoPlay width="640" height="480" />
-      </CameraPlaceholder>
-      <CaptureButton onClick={captureSnapshot}>Capture</CaptureButton>
-      <TranslationText>
-        <h2>Translation:</h2>
-        <p>{translation}</p>
-      </TranslationText>
-      {translation && <ClearButton onClick={handleClearTranslation}>Clear Translation</ClearButton>}
-      <Instructions>
-        <h2>Instructions:</h2>
-        <p>1. Place your hand in front of the camera.</p>
-        <p>2. Click the "Capture" button to capture a snapshot.</p>
-        <p>Note: This app only translates the alphabet for now.</p>
-      </Instructions>
-    </TranslationContainer>
-  );
-}
+    <>
+      <Analytics />
+      <NavContainer>
+        <BackgroundBlur />
+        <Nav>
+          <Logo>
+            <img src={ASL} alt="Logo" />
+            Salinterpret
+          </Logo>
+          <NavMenu className={menuOpen ? 'open' : ''}>
+            <NavItem onClick={() => scrollToSection('about')}>About</NavItem>
+            <NavItem onClick={() => scrollToSection('pricing')}>Pricing</NavItem>
+            <NavItem onClick={() => scrollToSection('features')}>Features</NavItem>
+            <NavItem onClick={() => scrollToSection('contact')}>Contact</NavItem>
+          </NavMenu>
+          <NavActions>
+            <MenuIcon onClick={handleMenuToggle} />
+          </NavActions>
+        </Nav>
 
-export default ASLTranslationPage;
+        <HeroSection>
+          <LeftSection>
+            <Title>Welcome!</Title>
+            <ButtonContainer>
+              <FreeTrialButton onClick={() => navigate('/login')}>Sign In</FreeTrialButton>
+              <SeeMoreButton onClick={onSeeMoreClick}>See More</SeeMoreButton>
+            </ButtonContainer>
+          </LeftSection>
+          <RightSection>
+            <LogoWrapper>
+              <KaliwaLogoImg src={KaliwaLogo} alt="Kaliwa Logo" />
+            </LogoWrapper>
+            <HeroTextContainer>
+              <Description>Salinterpret</Description>
+              <Text>
+                A web application that translates American Sign Language (ASL) into text in real-time. Using advanced computer vision technology, Salinterpret bridges the communication gap between the ASL community and non-signers, fostering inclusivity and understanding. Communicate easily and connect without barriers. 🌐✋🗨️
+              </Text>
+            </HeroTextContainer>
+          </RightSection>
+        </HeroSection>
+      </NavContainer>
+    </>
+  );
+};
+
+const AboutSection = () => {
+  const { ref: sectionRef, inView } = useInView({ triggerOnce: true });
+
+  return (
+    <Section id="about" ref={sectionRef}>
+      <h2>About Us</h2>
+      <ImagesContainer>
+        <ImageWrapper className={inView ? 'in-view' : ''}>
+          <img src={Hacker} alt="" />
+        </ImageWrapper>
+        <ImageWrapper className={inView ? 'in-view' : ''}>
+          <img src={Hispter} alt="" />
+        </ImageWrapper>
+        <ImageWrapper className={inView ? 'in-view' : ''}>
+          <img src={Member} alt="" />
+        </ImageWrapper>
+        <ImageWrapper className={inView ? 'in-view' : ''}>
+          <img src={Mentor} alt="" />
+        </ImageWrapper>
+      </ImagesContainer>
+      <p><strong><i>Meet the Team Numbros: The Team Behind Salinterpret</i></strong></p>
+      <p>
+        <ul>
+          <li>Justine Dimalanta: Hacker & Hustler, leading technical development</li>
+          <li>Lara Jane Acar: Hipster, designing an intuitive user experience.</li>
+          <li>Jerson Mamangun: Co-Hacker, refining the app’s performance.</li>
+          <li>Mr. Chris Allen Pineda: Project Adviser, guiding our mission.</li>
+        </ul>
+      </p>
+    </Section>
+  );
+};
+
+const PricingSection = () => (
+  <Section id="pricing">
+    <p style={{ fontSize: '28px' }}> <strong>Surprise!!! </strong></p>
+    <p>
+      Salinterpret is a free web app designed to bridge communication gaps. Our app offers essential features without any cost, ensuring that you have access to real-time translation and sign-to-word language features at no charge. You just need to have mobile data to access the app. Whether you're looking to communicate with the hearing-impaired or enhance your own understanding, Salinterpret is here to support you without any fees. Explore our features and experience the power of inclusive communication today!
+    </p>
+  </Section>
+);
+
+const FeaturesSection = () => (
+  <Section id="features">
+    <h2>Features</h2>
+    <p>Explore the features of Salinterpret...</p>
+  </Section>
+);
+
+const ContactSection = () => (
+  <Section id="contact">
+    <h2>Contact Us</h2>
+    <p>Have questions or feedback? Reach out to us...</p>
+  </Section>
+);
+
+const Popup = ({ show, onClose }) => {
+  if (!show) return null;
+
+  return (
+    <PopupOverlay>
+      <PopupContainer>
+        <CloseButton onClick={onClose}>Close</CloseButton>
+        <h2>Salinterpret</h2>
+        <p>We're on a mission to connect hearing-impaired and non-hearing-impaired communities like never before. With Salinterpret, sign language transforms into words, making communication effortless and inclusive. Dive into our ASL courses, tutorials, and fun interactive games that make learning exciting and accessible for everyone.
+          Join us in building a world where everyone can connect and understand each other, one sign at a time! ✋💬</p>
+      </PopupContainer>
+    </PopupOverlay>
+  );
+};
+
+// Styles
+const NavContainer = styled.div`
+  position: relative;
+  height: 100vh;
+  overflow: auto;
+  position: sticky;
+  scroll-snap-type: y mandatory;
+`;
+
+const BackgroundBlur = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url(${BackgroundImage}) no-repeat center center/cover;
+  filter: blur(3px);
+  z-index: -1;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  color: white;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  z-index: 100;
+`;
+
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  img {
+    width: 40px;
+    margin-right: 10px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+  list-style: none;
+  &.open {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 60px;
+    right: 20px;
+    background: white;
+    color: black;
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavItem = styled.li`
+  margin: 0 20px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    color: yellow;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    margin: 10px 0;
+  }
+`;
+
+const NavActions = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 768px) {
+    .sign-in-button {
+      display: none;
+    }
+  }
+`;
+
+const MenuIcon = styled(FaBars)`
+  display: none;
+  cursor: pointer;
+  font-size: 24px;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const HeroSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  padding: 0 100px;
+  position: relative;
+  flex-wrap: wrap;
+
+  @media (max-width: 768px) {
+    padding: 0 20px;
+    flex-direction: column;
+    text-align: center;
+  }
+`;
+
+const LeftSection = styled.div`
+  width: 50%;
+  padding-top: 50px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const FreeTrialButton = styled.button`
+  background-color: #ff6600;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const SeeMoreButton = styled.button`
+  background-color: transparent;
+  color: #ff6600;
+  padding: 10px 20px;
+  border: 2px solid #ff6600;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const RightSection = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding-left: 50px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding-left: 0;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+const KaliwaLogoImg = styled.img`
+  width: 250px;
+`;
+
+const HeroTextContainer = styled.div`
+  max-width: 400px;
+`;
+
+const Description = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Text = styled.p`
+  font-size: 16px;
+  line-height: 1.5;
+`;
+
+const Section = styled.section`
+  padding: 50px 20px;
+`;
+
+const ImagesContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const ImageWrapper = styled.div`
+  transition: transform 0.3s ease;
+  
+  &.in-view {
+    transform: scale(1.1);
+  }
+`;
+
+const PopupOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PopupContainer = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  width: 400px;
+  text-align: center;
+`;
+
+const CloseButton = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+export default Navbar;
