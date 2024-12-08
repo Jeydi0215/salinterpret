08f7cbfa4480604c6e790214e1e0cd801711abae
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-12-08 20:22:13 +0800

    flask

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -74,7 +74,7 @@ function ASLTranslationPage() {
     const fetchData = async () => {
       try {
         // Replace localhost URL with your Azure Dev Tunnels or correct backend URL
-        const response = await fetch('http://127.0.0.1:5000/translate', {
+        const response = await fetch('https://flasky-d9sr.onrender.com/translate', {
           headers: {
             'Content-Type': 'application/json',
           },
