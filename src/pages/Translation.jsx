88e5027353c3b734ecc5d14fda1879f71e3e9a44
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2025-01-14 13:39:08 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -77,7 +77,7 @@ function ASLTranslationPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://192.168.100.58:5000/translate');
+        const response = await fetch('https://f634-143-44-224-49.ngrok-free.app/translate');
         if (!response.ok) {
           throw new Error('Failed to fetch');
         }
