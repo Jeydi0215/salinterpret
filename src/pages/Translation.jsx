0468cffed8a44fa4f7632314610b4b38fef63383
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2025-04-11 13:14:02 +0800

    uwu

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -52,7 +52,7 @@ function CameraTranslator() {
   const [word, setWord] = useState("");
   const [error, setError] = useState("");
 
-  const serverUrl = "https://7bdd-143-44-145-81.ngrok-free.app"; // 🔁 update to match your ngrok
+  const serverUrl = "https://a91d-143-44-145-81.ngrok-free.app"; // 🔁 update to match your ngrok
 
   useEffect(() => {
     const startCamera = async () => {
