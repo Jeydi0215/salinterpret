10a5e5cfbdd4f0fb51caec238adc716d900b2ff0
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2025-05-13 22:13:07 +0800

    Update Translation.jsx

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -96,7 +96,7 @@ function ASLTranslator() {
   const canvasRef = useRef(document.createElement("canvas"));
   const [translation, setTranslation] = useState("");
   const [lastLetter, setLastLetter] = useState("");
-  const ngrokBase = "https://8217-143-44-224-17.ngrok-free.app"; // replace with your ngrok URL
+  const ngrokBase = "https://4528-143-44-145-19.ngrok-free.app"; // replace with your ngrok URL
 
   useEffect(() => {
     let stream;
