69c924a18d20d89c22cdd44591e77764b93537cd
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2025-05-21 14:22:31 +0800

    bagong ngrok

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -145,7 +145,7 @@ function ASLTranslator() {
   const [mode, setMode] = useState("words");
   const [currentPrediction, setCurrentPrediction] = useState({ letter: "", probability: 0 });
 
-  const ngrokBase = "https://3e0c-175-176-11-57.ngrok-free.app"; // <-- your flask URL
+  const ngrokBase = " https://2d49-175-176-11-57.ngrok-free.app"; // <-- your flask URL
 
   const getModelURLs = () => ({
     modelURL: "https://teachablemachine.withgoogle.com/models/AgwPr5b46/model.json",
