e32883c50f8e60340367f868256dadf7093f6bd8
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2025-06-02 14:12:08 +0800

    new links

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -428,7 +428,7 @@ function ASLTranslator() {
   const [showInstructions, setShowInstructions] = useState(true);
   const [status, setStatus] = useState('ready');
 
-  const ngrokBase = "https://c8eb-143-44-224-17.ngrok-free.app";
+  const ngrokBase = "https://723b-175-176-11-22.ngrok-free.app";
   const confidenceThreshold = 0.4;
 
   // Single function to handle API calls with proper throttling
